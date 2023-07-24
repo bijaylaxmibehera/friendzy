2f54be23d7168b4a0cdf95cb02c712cd38b92cfa
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2023-07-24 06:32:05 +0530

    feat: add users reducer

---
+++ src/reducer/usersReducer.jsx
@@ -0,0 +1,52 @@
+export const initialState = {
+  users: [],
+  bookmarks: [],
+  user: {},
+};
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "INITIALIZE_ALL_USERS":
+      return {
+        ...state,
+        users: [...action.payload],
+      };
+    case "INITIALIZE_ALL_BOOKMARKS":
+      return {
+        ...state,
+        bookmarks: [...action.payload],
+      };
+    case "ADD_TO_BOOKMARK":
+      return {
+        ...state,
+        bookmarks: [...action.payload],
+      };
+    case "REMOVE_FROM_BOOKMARK":
+      return {
+        ...state,
+        bookmarks: [...action.payload],
+      };
+    case "UPDATE_FOLLOW_USER":
+      const { user, followUser } = action.payload;
+      return {
+        ...state,
+        users: state.users.map((currentUser) =>
+          currentUser.username === user.username
+            ? { ...user }
+            : currentUser.username === followUser.username
+            ? { ...followUser }
+            : currentUser
+        ),
+      };
+    case "EDIT_USER_PROFILE":
+      return {
+        ...state,
+        users: state?.users.map((user) =>
+          user._id === action.payload._id ? action.payload : user
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
